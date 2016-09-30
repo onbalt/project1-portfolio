f38a868c798841c11e71ae614628c3b648fc380e
onbalt <github@onbalt.ru>
2016-10-01 02:15:38 +0300

    Prefinished about page with animated skills circles based on SVG path stroke-dashoffset

--- source/js/app.js
+++ source/js/app.js
@@ -2,6 +2,25 @@
   'use strict';
 
   setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
+    //document.querySelector('.greating_picture').classList.add('m--show');
   }, 1000);
 })();
+
+$(document).ready(function() {
+
+  $(document).on('click', '.menu__open-link, .menu__close-link', function(e){
+    e.preventDefault();
+    $(this).parents('.menu').toggleClass('menu_closed');
+  });
+
+  $.each($('path.skills__circle-svg-path'), function(i, path) {
+    var percent = $(path).data('percent'),
+        pathLength = path.getTotalLength(),
+        pathPercentLength = (100 - percent) * pathLength / 100;
+    $(path).css({'stroke-dasharray': pathLength, 'stroke-dashoffset': pathLength});
+    setTimeout(function() {
+      $(path).css({'opacity': '.'+percent, 'stroke-dashoffset': pathPercentLength});
+    }, 400 * (i + 3));
+  });
+
+});
